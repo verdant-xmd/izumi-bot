845f7ed720d2eb8495650650d5dbd0505cf2f72b
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-04-25 06:28:08 +0530

    fix

--- plugins/vv.js
+++ plugins/vv.js
@@ -1,4 +1,23 @@
-const { izumi,mode } = require('../lib/');
+const { izumi, mode, setWarn, removeWarn } = require('../lib/');
+const { WARN_COUNT } = require("../config")
+
+izumi({
+	pattern: 'warn',
+	fromMe: true,
+	desc: 'warn user',
+	type: 'group'
+}, async (message, match) => {
+	await setWarn(message, match, WARN_COUNT)
+});
+
+izumi({
+	pattern: 'resetwarn',
+	fromMe: true,
+	desc: 'warn user',
+	type: 'group'
+}, async (message, match) => {
+	await removeWarn(message)
+});
 izumi(
   {
     pattern: "vv",
