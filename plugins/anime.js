738d9ff382e586c1514e5b78e157f6d7a2eb3443
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-05-01 00:20:52 +0530

    anime

--- plugins/anime.js
+++ plugins/anime.js
@@ -50,3 +50,69 @@ izumi({
   const { url } = await getJson("https://api.waifu.pics/sfw/megumin");
   await message.sendFromUrl(url, { caption: config.CAPTION });
 });
+
+izumi ({
+    pattern: "naruto",
+    fromMe: mode,
+    desc: "random Naruto anime videos",
+    type: "Anime",
+}, async (message, match) => {
+  const { result } = await getJson(eypzApi + 'naruto');
+  message.sendFromUrl(result.url, {
+    contextInfo: {
+      mentionedJid: [message.sender],
+      forwardingScore: 1,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363298577467093@newsletter',
+        newsletterName: "Iᴢᴜᴍɪ-ᴠ3",
+        serverMessageId: -1
+      }
+    },
+    caption: `${config.CAPTION}`
+  });
+});
+
+izumi ({
+    pattern: "onepiece",
+    fromMe: mode,
+    desc: "random one-piece anime videos",
+    type: "Anime",
+}, async (message, match) => {
+const url = apiUrl + 'api/anime/onepiece?apikey=izumi-v3'
+  message.sendFromUrl(url, {
+    contextInfo: {
+      mentionedJid: [message.sender],
+      forwardingScore: 1,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363298577467093@newsletter',
+        newsletterName: "Iᴢᴜᴍɪ-ᴠ3",
+        serverMessageId: -1
+      }
+    },
+    caption: `${config.CAPTION}`
+  });
+});
+
+izumi ({
+    pattern: "demonslayer",
+    fromMe: mode,
+    desc: "random demonslayer anime video",
+    type: "Anime",
+}, async (message, match) => {
+const url = apiUrl + 'api/anime/demonslayer?apikey=izumi-v3'
+  message.sendFromUrl(url, {
+    contextInfo: {
+      mentionedJid: [message.sender],
+      forwardingScore: 1,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363298577467093@newsletter',
+        newsletterName: "Iᴢᴜᴍɪ-ᴠ3",
+        serverMessageId: -1
+      }
+    },
+    caption: `${config.CAPTION}`
+  });
+});
