b7cd7a6053ab0e1708eb040dada884ad851ac457
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-05-08 15:18:37 +0530

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -2,7 +2,6 @@ const { izumi, mode, blackVideo } = require("../lib/");
 const axios = require('axios');
 const crypto = require('crypto');
 const fs = require('fs');
-const fileType = require('file-type');
 const path = require('path');
 
 izumi({
@@ -21,45 +20,29 @@ izumi({
 
     if (message.quoted.audio) {
       const audioPath = await message.quoted.download();
-      const videoBuffer = await blackVideo(audioPath);
+      mediaBuffer = await blackVideo(audioPath); // Convert to black video
       fs.unlinkSync(audioPath);
-      mediaBuffer = videoBuffer;
       ext = 'mp4';
     } else {
       mediaBuffer = await message.quoted.download('buffer');
-
-      if (message.quoted.image) {
-        ext = 'png';
-      } else {
-        const hash = crypto.createHash('sha256').update(mediaBuffer).digest('hex');
-        const tempPath = `/tmp/${hash}`;
-        fs.writeFileSync(tempPath, mediaBuffer);
-
-        const type = await fileType.fromFile(tempPath);
-        ext = type && ['jpg', 'jpeg', 'png', 'mp4', 'mp3'].includes(type.ext)
-          ? (type.ext === 'jpeg' ? 'jpg' : type.ext)
-          : 'bin';
-
-        fs.unlinkSync(tempPath);
-      }
+      ext = message.quoted.image ? 'png' : message.quoted.video ? 'mp4' : 'bin';
     }
 
     const hash = crypto.createHash('sha256').update(mediaBuffer).digest('hex');
+    const filename = `${hash}.${ext}`;
     const base64 = mediaBuffer.toString('base64');
 
     const { data: uploadRes } = await axios.post('https://cdn.eypz.ct.ws/upload', {
-      base64, ext, filename: hash
+      base64, ext, filename
     });
 
-    const fileId = Object.values(uploadRes.result).find(x => x?.file_id)?.file_id;
+    const url = uploadRes?.url || uploadRes?.result?.url;
 
-    if (!fileId) {
-      return await message.reply('Upload succeeded, but no file ID returned.');
+    if (!url) {
+      return await message.reply('Upload failed: No URL returned.');
     }
 
-    const { data } = await axios.get(`https:cdn.eypz.ct.ws/getFileUrl?fileId=${fileId}`);
-
-    await message.reply(data.streamUrl || 'URL not found.');
+    await message.reply(url);
   } catch (err) {
     const errorMsg = err.response?.data?.message || err.message || 'Something went wrong.';
     await message.reply(`Failed: ${errorMsg}`);
