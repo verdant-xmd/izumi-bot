b9dff9ce8db25d7ae2061a6c4cd214109797da48
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-05-21 03:53:01 +0530

    insta fix

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,50 +1,45 @@
 const { izumi, mode } = require("../lib");
 const fetch = require("node-fetch");
-const config = require("../config");
-const caption = config.CAPTION;
  izumi({
     pattern: 'insta ?(.*)',
     fromMe: mode,
-    desc: 'Download all Instagram media (reels/images)',
-    type: 'downloader'
+    desc: 'Download instagram videos/image.',
+    type: 'downloader',
 }, async (message, match, client) => {
-    if (!match) {
-        return await message.reply('Please provide an Instagram URL!');
-    }
+    try {
+        const url = match || message.reply_message.text;
+        if (!url) {
+            return await message.reply("Please provide a valid Instagram URL.");
+        }
 
-   const api = `https://fastrestapis.fasturl.cloud/downup/igdown/advanced?url=${match}&type=simple`;
+        const igApi = `https://api.siputzx.my.id/api/d/igdl?url=${url}`;
+        const res = await fetch(igApi);
+        if (!res.ok) {
+            return await message.reply("Please try again.");
+        }
+        
+        const data = await res.json();
+        const igmedia = data.data;
 
-(async () => {
-  try {
-    const response = await fetch(api);
-    const data = await response.json();
-    const result = data?.result;
-
-    const images = result.images || [];
-    const videos = result.videos || [];
-
-    
-    for (const url of images) {
-      await message.client.sendMessage(message.chat, {
-        image: { url },
-        caption: caption
-      }, { quoted: message.data });
-    }
-
-    
-    for (const url of videos) {
-      await message.client.sendMessage(message.chat, {
-        video: { url },
-        caption: caption
-      }, { quoted: message.data });
-    }
-  } catch (err) {
-    console.error("Error fetching media:", err);
-    await message.client.sendMessage(message.chat, {
+        if (igmedia && igmedia.length > 0) {
+            let counter = 0;
+            for (const media of igmedia) {
+                if (counter >= 10) break;
+                const mediaurl = media.url;
+                await message.sendFile(mediaurl, {quoted: message.data});
+                counter++;
+            }
+        } else {
+            await message.client.sendMessage(client.user.id, {
       text: "Error fetching media"
     }, { quoted: message.data });
-  }
-})();
+        }
+    } catch (error) {
+        console.error(error);
+        await message.client.sendMessage(client.user.id, {
+      text: error
+    }, { quoted: message.data });
+    }
 });
 izumi({
     pattern: 'fb ?(.*)',
@@ -55,7 +50,7 @@ izumi({
     try {
         const url = match || message.reply_message.text;
         if (!url) {
-            return await message.reply("Please provide a valid Instagram URL.");
+            return await message.reply("Please provide a valid facebook URL.");
         }
 
         const fbApi = `https://api.siputzx.my.id/api/d/igdl?url=${url}`;
