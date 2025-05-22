2d2b8b0e910703ef131eaa9588e15c38ff5e0bff
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-05-23 05:18:37 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -5,11 +5,13 @@ izumi({
     desc: 'text to image',
     type: 'ai',
 }, async (message, match, client) => {
+const text = match || (message.reply_message && message.reply_message.text);
+if (!text) return await message.reply("_*Need a query*_\nexample: _*.flux A realistic portrait of a Japanese teenage girl with white hair, sitting in front of a high-end gaming PC, playing GTA V. She is focused on the screen, wearing casual modern clothes, in a cozy gaming room with RGB lighting and gaming accessories around her_");
 await message.client.sendMessage(
   message.jid,
   {
-    image: { url: `https://api.eypz.ct.ws/api/ai/flux?prompt=${match}`},
-    caption: match,
+    image: { url: `https://api.eypz.ct.ws/api/ai/flux?prompt=${text}`},
+    caption: text,
     contextInfo: {
       externalAdReply: {
         title: "AI Generated Image",
