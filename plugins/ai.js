590ecb4e237efbbe787d474f1707cd24fc497f45
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-05-25 02:49:20 +0530

    gpt3, gpt4

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,4 +1,104 @@
-const { izumi, mode } = require("../lib");
+const { izumi, getJson, mode } = require("../lib");
+izumi({
+    pattern: 'gpt3 ?(.*)',
+    fromMe: mode,
+    desc: 'chat got',
+    type: 'ai',
+}, async (message, match, client) => {
+    const text = match || (message.reply_message && message.reply_message.text);
+if (!text) return await message.reply("_*Need a query*_");
+const data = await getJson(`https://api.eypz.ct.ws/api/ai/gpt?text=${text}&model=gpt3`);
+const quotedMessage = {
+  key: {
+    fromMe: false,
+    remoteJid: "18002428478@s.whatsapp.net",
+    id: "FAKE_MESSAGE_ID",
+    participant: "18002428478@s.whatsapp.net"
+  },
+  message: {
+    extendedTextMessage: {
+      text: "GPT-3"
+    }
+  }
+};
+
+await client.sendMessage(
+  message.jid,
+  {
+    text: data.response,
+    contextInfo: {
+      mentionedJid: [message.sender],
+      forwardingScore: 1,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: "120363298577467093@newsletter",
+        newsletterName: "GPT",
+        serverMessageId: -1
+      },
+      externalAdReply: {
+        title: "Chat Gpt 3",
+        body: "Powered by Eypz API",
+        mediaType: 2,
+        thumbnailUrl: "https://files.catbox.moe/saleb0.png",
+        showAdAttribution: true,
+        mediaUrl: "https://eypz.rf.gd",
+        sourceUrl: "https://eypz.rf.gd"
+      }
+    }
+  },
+  { quoted: quotedMessage }
+)
+});
+izumi({
+    pattern: 'gpt4 ?(.*)',
+    fromMe: mode,
+    desc: 'chat got',
+    type: 'ai',
+}, async (message, match, client) => {
+    const text = match || (message.reply_message && message.reply_message.text);
+if (!text) return await message.reply("_*Need a query*_");
+const data = await getJson(`https://api.eypz.ct.ws/api/ai/gpt?text=${text}&model=gpt4`);
+const quotedMessage = {
+  key: {
+    fromMe: false,
+    remoteJid: "18002428478@s.whatsapp.net",
+    id: "FAKE_MESSAGE_ID",
+    participant: "18002428478@s.whatsapp.net"
+  },
+  message: {
+    extendedTextMessage: {
+      text: "GPT-4"
+    }
+  }
+};
+
+await client.sendMessage(
+  message.jid,
+  {
+    text: data.response,
+    contextInfo: {
+      mentionedJid: [message.sender],
+      forwardingScore: 1,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: "120363298577467093@newsletter",
+        newsletterName: "GPT",
+        serverMessageId: -1
+      },
+      externalAdReply: {
+        title: "Chat Gpt 4",
+        body: "Powered by Eypz API",
+        mediaType: 2,
+        thumbnailUrl: "https://files.catbox.moe/saleb0.png",
+        showAdAttribution: true,
+        mediaUrl: "https://eypz.rf.gd",
+        sourceUrl: "https://eypz.rf.gd"
+      }
+    }
+  },
+  { quoted: quotedMessage }
+)
+});
 izumi({
     pattern: 'flux ?(.*)',
     fromMe: mode,
