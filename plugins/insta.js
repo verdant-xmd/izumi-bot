0c5352fcd742785e0ae26a1847848a4e6459f08d
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-06-01 16:15:52 +0530

    insta fix

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,44 +1,34 @@
 const { izumi, mode } = require("../lib");
-const fetch = require("node-fetch");
- izumi({
+const axios = require("axios");
+
+izumi({
     pattern: 'insta ?(.*)',
     fromMe: mode,
     desc: 'Download instagram videos/image.',
     type: 'downloader',
 }, async (message, match, client) => {
     try {
-        const url = match || message.reply_message.text;
-        if (!url) {
-            return await message.reply("Please provide a valid Instagram URL.");
+        const url = match || message.reply_message?.text;
+        if (!url) return await message.reply("Please provide a valid Instagram URL.");
+
+        const api = `https://api.eypz.ct.ws/api/dl/instagram?url=${url}`;
+        const res = await axios.get(api);
+
+        const mediaList = res.data?.result?.data;
+        if (!mediaList || mediaList.length === 0) {
+            return await message.reply("No media found.");
         }
 
-        const igApi = `https://api.eypz.ct.ws/api/dl/instagram?url=${url}`;
-        const res = await fetch(igApi);
-        if (!res.ok) {
-            return await message.reply("Please try again.");
-        }
-        
-        const data = await res.json();
-        const igmedia = data.data;
-
-        if (igmedia && igmedia.length > 0) {
-            let counter = 0;
-            for (const media of igmedia) {
-                if (counter >= 10) break;
-                const mediaurl = media.url;
-                await message.sendFile(mediaurl, {quoted: message.data});
-                counter++;
+        for (const media of mediaList) {
+            if (media.url) {
+                
+                await message.sendFile(media.url, {}, { quoted: message.data });
             }
-        } else {
-            await message.client.sendMessage(client.user.id, {
-      text: "Error fetching media"
-    }, { quoted: message.data });
         }
-    } catch (error) {
-        console.error(error);
-        await message.client.sendMessage(client.user.id, {
-      text: error
-    }, { quoted: message.data });
+
+    } catch (err) {
+        console.error(err);
+        await client.sendMessage(client.user.id, { text: "Error: " + err.message });
     }
 });
 izumi({
