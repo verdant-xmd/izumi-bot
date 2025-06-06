304c8d227ceafab32fedd7a60e441cf1a82d9fa3
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-06-07 03:14:26 +0530

    3.9.2

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -1,106 +1,21 @@
-const { PREFIX, izumi, mode, commands } = require("../lib/");
-const version = require("../package.json").version;
-const config = require("../config");
-
+const { izumi, mode ,sendMenu, sendSegMenu} = require("../lib/");
 izumi({
-    pattern: 'menu ?(.*)',
+    pattern: "menu ?(.*)",
+    desc: "izumi-v3 user manual",
     fromMe: mode,
-    desc: 'Displays the bot menu with commands categorized by type.',
-    type: 'info'
+    type: "user",
 }, async (message, match, client) => {
-
-const readMore = String.fromCharCode(8206).repeat(4001);
-const quotedMessage = {
-    key: {
-        fromMe: false,
-        participant: "0@s.whatsapp.net",
-        remoteJid: "120363413452172651@g.us",
-    },
-    message: {
-        orderMessage: {
-            orderId: "2009",
-            itemCount: 3000,
-            status: "INQUIRY",
-            surface: "",
-            message: `ϟ ${config.BOT_NAME}`,
-            token: Buffer.from("izumi").toString("base64"),
-        },
-    },
-    contextInfo: {
-        forwardingScore: 999,
-        isForwarded: true,
-    },
-};
-
-let menu = `
-┏━━━〔 ${config.BOT_NAME} 〕━━━╮
-┃
-┃
-┃ ⤑ Owner   : ${config.OWNER_NAME}
-┃ ⤑ Commands: ${commands.filter((command) => command.pattern).length}
-┃ ⤑ Mode    : ${mode ? 'PRIVATE' : 'PUBLIC'}
-┃ ⤑ Prefix  : ${PREFIX}
-┃ ⤑ Version : ${version}
-┗━━━━━━━━━━━━${readMore}`;
-
-let cmnd = [];
-let category = [];
-
-commands.forEach((command) => {
-    let cmd;
-    if (command.pattern instanceof RegExp) {
-        cmd = String(command.pattern).split(/\W+/)[1];
-    }
-
-    if (!command.dontAddCommandList && command.pattern) {
-        let type = (command.type || "misc").toString();
-        cmnd.push({ cmd, type });
-
-        if (!category.includes(type)) category.push(type);
-    }
+    await sendMenu(message, match, client);
 });
 
-cmnd.sort();
-category.sort().forEach((cmmd) => {
-    menu += `\n\n┃  ╭─────────────•`;
-    menu += `\n┃  │  *ᯓ${cmmd.toString().toUpperCase()}*`;
-    menu += `\n┃  ╰─────────────•`;
+const pluginTypes = ['downloader', 'group', 'media', 'react', 'user', 'converter', 'ai'];
 
-    let comad = cmnd.filter(({ type }) => type === cmmd);
-    comad.forEach(({ cmd }) => {
-        menu += `\n┃  ⤑ ${cmd ? cmd.trim() : ''}`;
+pluginTypes.forEach((type) => {
+    izumi({
+        pattern: `${type}$`,
+        fromMe: mode,
+        dontAddCommandList: true,
+    }, async (message, match, client ) => {
+        await sendSegMenu(message, match, client, type);
     });
-});
-
-menu += `\n |
- |
-ׂ╰────────────┈➤`;
-
-let mediaUrl = config.MENU_URL;
-
-let mimetype, type;
-if (mediaUrl.endsWith(".mp4")) {
-    mimetype = "video/mp4";
-    type = "video";
-} else if (mediaUrl.endsWith(".png") || mediaUrl.endsWith(".jpg") || mediaUrl.endsWith(".jpeg")) {
-    mimetype = mediaUrl.endsWith(".png") ? "image/png" : "image/jpeg";
-    type = "image";
-}
-
-await message.client.sendMessage(
-    message.jid,
-    {
-        [type]: { url: mediaUrl },
-        mimetype,
-        caption: menu,
-        gifPlayback: true,
-        contextInfo: {
-            isForwarded: true,
-            forwardingScore: 999
-        }
-    },
-    {
-        quoted: quotedMessage
-    }
- )
 });
