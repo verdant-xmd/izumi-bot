d270ed132e1ea220a99ef4ea33f889b19270de34
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-06-09 00:06:14 +0530

    anime fix

--- plugins/anime.js
+++ plugins/anime.js
@@ -139,27 +139,6 @@ izumi({
   await message.sendFromUrl(url, { caption: config.CAPTION });
 });
 
-izumi ({
-    pattern: "naruto",
-    fromMe: mode,
-    desc: "random Naruto anime videos",
-    type: "Anime",
-}, async (message, match) => {
-  const { result } = await getJson(eypzApi + 'naruto');
-  message.sendFromUrl(result.url, {
-    contextInfo: {
-      mentionedJid: [message.sender],
-      forwardingScore: 1,
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363298577467093@newsletter',
-        newsletterName: "Iᴢᴜᴍɪ-ᴠ3",
-        serverMessageId: -1
-      }
-    },
-    caption: `${config.CAPTION}`
-  });
-});
 
 izumi ({
     pattern: "onepiece",
@@ -167,40 +146,20 @@ izumi ({
     desc: "random one-piece anime videos",
     type: "Anime",
 }, async (message, match) => {
-const url = apiUrl + 'api/anime/onepiece?apikey=izumi-v3'
-  message.sendFromUrl(url, {
-    contextInfo: {
-      mentionedJid: [message.sender],
-      forwardingScore: 1,
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363298577467093@newsletter',
-        newsletterName: "Iᴢᴜᴍɪ-ᴠ3",
-        serverMessageId: -1
-      }
-    },
+const url = apiUrl + 'https://izumi-api.vercel.app/api/onepiece'
+  message.sendFromUrl(url, {},
     caption: `${config.CAPTION}`
   });
 });
 
 izumi ({
-    pattern: "demonslayer",
+    pattern: "naruto$",
     fromMe: mode,
     desc: "random demonslayer anime video",
     type: "Anime",
 }, async (message, match) => {
-const url = apiUrl + 'api/anime/demonslayer?apikey=izumi-v3'
-  message.sendFromUrl(url, {
-    contextInfo: {
-      mentionedJid: [message.sender],
-      forwardingScore: 1,
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363298577467093@newsletter',
-        newsletterName: "Iᴢᴜᴍɪ-ᴠ3",
-        serverMessageId: -1
-      }
-    },
+const url = apiUrl + 'https://izumi-api.vercel.app/api/naruto'
+  message.sendFromUrl(url, {},
     caption: `${config.CAPTION}`
   });
 });
