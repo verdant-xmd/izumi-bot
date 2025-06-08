b23b8101270e1323ef692d1f9e5f12ada3c638d5
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-06-09 00:57:29 +0530

    react

--- plugins/react.js
+++ plugins/react.js
@@ -1,7 +1,10 @@
+const axios = require('axios');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
 const { izumi, mode, parsedJid } = require("../lib/");
 const gif = "https://api.eypz.ct.ws/api/gif/";
-
-// Surprise Reaction
+const tempGif = "./temp/temp.gif";
+const tempMp4 = "./temp/converted.mp4";
 izumi({
     pattern: "surprise ?(.*)",
     fromMe: mode,
@@ -311,4 +314,305 @@ izumi({
         },
         { quoted: message.quoted.data || message.data }
     );
+});
+
+izumi({
+    pattern: "handhold ?(.*)",
+    fromMe: mode,
+    desc: "react with handhold gif",
+    type: "react",
+}, async (message, match, client) => {
+match = match || message.reply_message?.sender || message.sender;
+    const cleanJid = match.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+    const jid = parsedJid(cleanJid);
+    const jidString = Array.isArray(jid) ? jid[0] : jid;
+if (!fs.existsSync('./temp')) fs.mkdirSync('./temp');
+const response = await axios.get(gif+"handhold", { responseType: 'stream' });
+await new Promise((resolve, reject) => {
+  const writer = fs.createWriteStream(tempGif);
+  response.data.pipe(writer)
+  writer.on('finish', resolve);
+  writer.on('error', reject);
+});
+await new Promise((resolve, reject) => {
+  ffmpeg(tempGif)
+    .outputOptions([
+      '-movflags faststart',
+      '-pix_fmt yuv420p',
+      '-vf scale=trunc(iw/2)*2:trunc(ih/2)*2'
+    ])
+    .toFormat('mp4')
+    .save(tempMp4)
+    .on('end', resolve)
+    .on('error', reject);
+});
+
+await message.client.sendMessage(message.jid, {
+  video: { url: tempMp4 },
+  mimetype: 'video/mp4',
+  gifPlayback: true,
+  caption: `_@${jidString.split("@")[0]}_`,
+  mentions: [jidString],
+  },
+ { quoted: message.quoted.data || message.data });
+fs.unlinkSync(tempGif);
+fs.unlinkSync(tempMp4);
+});
+
+izumi({
+    pattern: "kill ?(.*)",
+    fromMe: mode,
+    desc: "react with kill gif",
+    type: "react",
+}, async (message, match, client) => {
+match = match || message.reply_message?.sender || message.sender;
+    const cleanJid = match.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+    const jid = parsedJid(cleanJid);
+    const jidString = Array.isArray(jid) ? jid[0] : jid;
+if (!fs.existsSync('./temp')) fs.mkdirSync('./temp');
+const response = await axios.get(gif+"Kill", { responseType: 'stream' });
+await new Promise((resolve, reject) => {
+  const writer = fs.createWriteStream(tempGif);
+  response.data.pipe(writer)
+  writer.on('finish', resolve);
+  writer.on('error', reject);
+});
+await new Promise((resolve, reject) => {
+  ffmpeg(tempGif)
+    .outputOptions([
+      '-movflags faststart',
+      '-pix_fmt yuv420p',
+      '-vf scale=trunc(iw/2)*2:trunc(ih/2)*2'
+    ])
+    .toFormat('mp4')
+    .save(tempMp4)
+    .on('end', resolve)
+    .on('error', reject);
+});
+
+await message.client.sendMessage(message.jid, {
+  video: { url: tempMp4 },
+  mimetype: 'video/mp4',
+  gifPlayback: true,
+  caption: `_@${jidString.split("@")[0]}_`,
+  mentions: [jidString],
+  },
+ { quoted: message.quoted.data || message.data });
+fs.unlinkSync(tempGif);
+fs.unlinkSync(tempMp4);
+});
+
+izumi({
+    pattern: "bonk ?(.*)",
+    fromMe: mode,
+    desc: "react with bonk gif",
+    type: "react",
+}, async (message, match, client) => {
+match = match || message.reply_message?.sender || message.sender;
+    const cleanJid = match.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+    const jid = parsedJid(cleanJid);
+    const jidString = Array.isArray(jid) ? jid[0] : jid;
+if (!fs.existsSync('./temp')) fs.mkdirSync('./temp');
+const response = await axios.get(gif+"bonk", { responseType: 'stream' });
+await new Promise((resolve, reject) => {
+  const writer = fs.createWriteStream(tempGif);
+  response.data.pipe(writer)
+  writer.on('finish', resolve);
+  writer.on('error', reject);
+});
+await new Promise((resolve, reject) => {
+  ffmpeg(tempGif)
+    .outputOptions([
+      '-movflags faststart',
+      '-pix_fmt yuv420p',
+      '-vf scale=trunc(iw/2)*2:trunc(ih/2)*2'
+    ])
+    .toFormat('mp4')
+    .save(tempMp4)
+    .on('end', resolve)
+    .on('error', reject);
+});
+
+await message.client.sendMessage(message.jid, {
+  video: { url: tempMp4 },
+  mimetype: 'video/mp4',
+  gifPlayback: true,
+  caption: `_@${jidString.split("@")[0]}_`,
+  mentions: [jidString],
+  },
+ { quoted: message.quoted.data || message.data });
+fs.unlinkSync(tempGif);
+fs.unlinkSync(tempMp4);
+});
+
+izumi({
+    pattern: "bully ?(.*)",
+    fromMe: mode,
+    desc: "react with bully gif",
+    type: "react",
+}, async (message, match, client) => {
+match = match || message.reply_message?.sender || message.sender;
+    const cleanJid = match.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+    const jid = parsedJid(cleanJid);
+    const jidString = Array.isArray(jid) ? jid[0] : jid;
+if (!fs.existsSync('./temp')) fs.mkdirSync('./temp');
+const response = await axios.get(gif+"bully", { responseType: 'stream' });
+await new Promise((resolve, reject) => {
+  const writer = fs.createWriteStream(tempGif);
+  response.data.pipe(writer)
+  writer.on('finish', resolve);
+  writer.on('error', reject);
+});
+await new Promise((resolve, reject) => {
+  ffmpeg(tempGif)
+    .outputOptions([
+      '-movflags faststart',
+      '-pix_fmt yuv420p',
+      '-vf scale=trunc(iw/2)*2:trunc(ih/2)*2'
+    ])
+    .toFormat('mp4')
+    .save(tempMp4)
+    .on('end', resolve)
+    .on('error', reject);
+});
+
+await message.client.sendMessage(message.jid, {
+  video: { url: tempMp4 },
+  mimetype: 'video/mp4',
+  gifPlayback: true,
+  caption: `_@${jidString.split("@")[0]}_`,
+  mentions: [jidString],
+  },
+ { quoted: message.quoted.data || message.data });
+fs.unlinkSync(tempGif);
+fs.unlinkSync(tempMp4);
+});
+
+izumi({
+    pattern: "lick ?(.*)",
+    fromMe: mode,
+    desc: "react with lick gif",
+    type: "react",
+}, async (message, match, client) => {
+match = match || message.reply_message?.sender || message.sender;
+    const cleanJid = match.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+    const jid = parsedJid(cleanJid);
+    const jidString = Array.isArray(jid) ? jid[0] : jid;
+if (!fs.existsSync('./temp')) fs.mkdirSync('./temp');
+const response = await axios.get(gif+"lick", { responseType: 'stream' });
+await new Promise((resolve, reject) => {
+  const writer = fs.createWriteStream(tempGif);
+  response.data.pipe(writer)
+  writer.on('finish', resolve);
+  writer.on('error', reject);
+});
+await new Promise((resolve, reject) => {
+  ffmpeg(tempGif)
+    .outputOptions([
+      '-movflags faststart',
+      '-pix_fmt yuv420p',
+      '-vf scale=trunc(iw/2)*2:trunc(ih/2)*2'
+    ])
+    .toFormat('mp4')
+    .save(tempMp4)
+    .on('end', resolve)
+    .on('error', reject);
+});
+
+await message.client.sendMessage(message.jid, {
+  video: { url: tempMp4 },
+  mimetype: 'video/mp4',
+  gifPlayback: true,
+  caption: `_@${jidString.split("@")[0]}_`,
+  mentions: [jidString],
+  },
+ { quoted: message.quoted.data || message.data });
+fs.unlinkSync(tempGif);
+fs.unlinkSync(tempMp4);
+});
+
+izumi({
+    pattern: "pat ?(.*)",
+    fromMe: mode,
+    desc: "react with pat gif",
+    type: "react",
+}, async (message, match, client) => {
+match = match || message.reply_message?.sender || message.sender;
+    const cleanJid = match.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+    const jid = parsedJid(cleanJid);
+    const jidString = Array.isArray(jid) ? jid[0] : jid;
+if (!fs.existsSync('./temp')) fs.mkdirSync('./temp');
+const response = await axios.get(gif+"pat", { responseType: 'stream' });
+await new Promise((resolve, reject) => {
+  const writer = fs.createWriteStream(tempGif);
+  response.data.pipe(writer)
+  writer.on('finish', resolve);
+  writer.on('error', reject);
+});
+await new Promise((resolve, reject) => {
+  ffmpeg(tempGif)
+    .outputOptions([
+      '-movflags faststart',
+      '-pix_fmt yuv420p',
+      '-vf scale=trunc(iw/2)*2:trunc(ih/2)*2'
+    ])
+    .toFormat('mp4')
+    .save(tempMp4)
+    .on('end', resolve)
+    .on('error', reject);
+});
+
+await message.client.sendMessage(message.jid, {
+  video: { url: tempMp4 },
+  mimetype: 'video/mp4',
+  gifPlayback: true,
+  caption: `_@${jidString.split("@")[0]}_`,
+  mentions: [jidString],
+  },
+ { quoted: message.quoted.data || message.data });
+fs.unlinkSync(tempGif);
+fs.unlinkSync(tempMp4);
+});
+
+izumi({
+    pattern: "slap ?(.*)",
+    fromMe: mode,
+    desc: "react with slap gif",
+    type: "react",
+}, async (message, match, client) => {
+match = match || message.reply_message?.sender || message.sender;
+    const cleanJid = match.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+    const jid = parsedJid(cleanJid);
+    const jidString = Array.isArray(jid) ? jid[0] : jid;
+if (!fs.existsSync('./temp')) fs.mkdirSync('./temp');
+const response = await axios.get(gif+"slap", { responseType: 'stream' });
+await new Promise((resolve, reject) => {
+  const writer = fs.createWriteStream(tempGif);
+  response.data.pipe(writer)
+  writer.on('finish', resolve);
+  writer.on('error', reject);
+});
+await new Promise((resolve, reject) => {
+  ffmpeg(tempGif)
+    .outputOptions([
+      '-movflags faststart',
+      '-pix_fmt yuv420p',
+      '-vf scale=trunc(iw/2)*2:trunc(ih/2)*2'
+    ])
+    .toFormat('mp4')
+    .save(tempMp4)
+    .on('end', resolve)
+    .on('error', reject);
+});
+
+await message.client.sendMessage(message.jid, {
+  video: { url: tempMp4 },
+  mimetype: 'video/mp4',
+  gifPlayback: true,
+  caption: `_@${jidString.split("@")[0]}_`,
+  mentions: [jidString],
+  },
+ { quoted: message.quoted.data || message.data });
+fs.unlinkSync(tempGif);
+fs.unlinkSync(tempMp4);
 });
