ac985924e0ab2dee43316473e29e6fd8f52ece61
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-06-12 04:53:12 +0530

    fix

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ function convertToBool(text, fault = 'true') {
 }
 
 const toBool = (x) => (x && x.toLowerCase() === 'true') || false;
-global.apiUrl = 'https://api.maskser.me/'
+global.apiUrl = 'api.eypz.ct.ws'
 global.eypzApi = 'https://api2.eypz.infy.uk/'
-global.Api = 'https://api.eypz.infy.uk/'
+global.Api = 'https://api-eypz-y068.onrender.com'
 
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL;
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
