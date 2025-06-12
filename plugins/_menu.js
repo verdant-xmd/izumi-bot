fd635b7a96310d7b5e8dc53fe8d1bd7e38d7e22b
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-06-13 04:30:33 +0530

    4.2.0

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -1,21 +1,33 @@
 const { izumi, mode ,sendMenu, sendSegMenu} = require("../lib/");
+const { handleAlive } = require("../lib/alive");
+izumi({
+  pattern: "alive ?(.*)",
+  fromMe: mode,
+  desc: "Handle .alive command",
+  type: "misc",
+}, async (message, match, client) => {
+  await handleAlive(message, match, client);
+});
+
 izumi({
     pattern: "menu ?(.*)",
     desc: "izumi-v3 user manual",
     fromMe: mode,
-    type: "user",
+    type: "misc",
 }, async (message, match, client) => {
     await sendMenu(message, match, client);
 });
 
+
 const pluginTypes = ['downloader', 'group', 'media', 'react', 'user', 'converter', 'ai'];
+
 
 pluginTypes.forEach((type) => {
     izumi({
-        pattern: `${type}$`,
+       pattern: `${type}$`,
         fromMe: mode,
-        dontAddCommandList: true,
+       dontAddCommandList: true,
     }, async (message, match, client ) => {
         await sendSegMenu(message, match, client, type);
-    });
+ });
 });
